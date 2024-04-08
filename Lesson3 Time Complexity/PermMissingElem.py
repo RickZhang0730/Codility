b396da27179c05e5e1bdd8a707db1dd8062218c3
RickZhang0730 <71380926+RickZhang0730@users.noreply.github.com>
2024-04-08 20:07:34 +0800

    Update PermMissingElem.py

--- Lesson3 Time Complexity/PermMissingElem.py
+++ Lesson3 Time Complexity/PermMissingElem.py
@@ -1,6 +1,6 @@
 def solution(A):
     N = len(A)
-    total_sum = (1 + (N + 1))*(N + 1) // 2
-    array_sum = sum(A)
-    x = total_sum - array_sum
+    total_sum = (1 + (N + 1))*(N + 1) // 2  #正常總和
+    array_sum = sum(A) #目前總和
+    x = total_sum - array_sum #找到所缺失的
     return x
